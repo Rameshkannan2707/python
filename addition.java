2889a5b9226aafaa39f2902f4edc2f6e1902f7b2
Rameshkannan2707 <51329799+Rameshkannan2707@users.noreply.github.com>
2020-05-06 11:17:36 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      int a,b;
+      Scanner scanner = new Scanner(System.in);
+      a=scanner.nextInt();
+      b=scanner.nextInt();
+      int c=a+b;
+      System.out.print(c);
+    }
+}
